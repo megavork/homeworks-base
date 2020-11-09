e8d72b6d2c4d7cc8ec49663434ee325ea9d28c2c
megavork <megavork@mail.ru>
2020-11-09 21:32:39 +0300

    was added 2 new methods.

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(1 > 0);
+    }
+
+    @Test
+    public void getMaxValue() {
+        int max = App.showMax(3, 9);
+        assertEquals(9, max);
+    }
+
+    @Test
+    public void getAnotherMaxValue() {
+        int max = App.showMax(9, 3);
+        assertEquals(9, max);
+    }
+
+    @Test
+    public void getAnotherMinValue() {
+        int max = App.showMax(9, 3);
+        assertEquals(3, max);
+    }
+}
