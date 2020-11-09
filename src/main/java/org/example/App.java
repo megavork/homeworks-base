73ab7cd86d22f5758406d3beb4d9a1a1011650fd
megavork <megavork@mail.ru>
2020-11-09 21:32:39 +0300

    was added 2 new methods.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,8 +7,4 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World.");
     }
-
-    public static int showMax(int i, int i1) {
-        return 0;
-    }
 }
