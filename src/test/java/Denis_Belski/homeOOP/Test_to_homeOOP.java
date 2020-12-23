d58a43fac0186b5c36212117c5fe6a93778216eb
megavork <megavork@mail.ru>
2020-12-23 18:32:34 +0300

    Tests was changed.

--- src/test/java/Denis_Belski/homeOOP/Test_to_homeOOP.java
+++ src/test/java/Denis_Belski/homeOOP/Test_to_homeOOP.java
@@ -13,39 +13,12 @@ public class Test_to_homeOOP {
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerZeroOff() {
-        homeOOP_Computer task = new homeOOP_Computer();
-        int result = task.powerOff(0);
-        assertEquals(0,result);
-    }
-
-    @Test
-    public void shouldAnswerOneOff() {
-        homeOOP_Computer task = new homeOOP_Computer();
-        int result = task.powerOff(1);
-        assertEquals(0,result);
-    }
 
     @Test
     public void shouldAnswerMinesOneOff() {
         homeOOP_Computer task = new homeOOP_Computer();
         int result = task.powerOff(6);
         assertEquals(-1,result);
-    }
-
-    @Test
-    public void shouldAnswerZeroOn() {
-        homeOOP_Computer task = new homeOOP_Computer();
-        int result = task.powerOn(0);
-        assertEquals(0,result);
-    }
-
-    @Test
-    public void shouldAnswerOneOn() {
-        homeOOP_Computer task = new homeOOP_Computer();
-        int result = task.powerOn(1);
-        assertEquals(0,result);
     }
 
     @Test
